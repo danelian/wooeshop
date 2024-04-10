9afdd43ae30662ab555745b1290fe01b1bce3eb4
danelian <141317269+danelian@users.noreply.github.com>
2024-04-10 18:30:30 +0400

    hooks

--- functions.php
+++ functions.php
@@ -1,5 +1,23 @@
 <?php 
 
-add_action('after_sutup_theme', function() {
+add_action('after_setup_theme', function() {
   add_theme_support('woocommerce');
+
+  add_theme_support('wc-product-gallery-zoom');
+  add_theme_support('wc-product-gallery-lightbox');
+  add_theme_support('wc-product-gallery-slider');
 });
+
+// change content wrapper
+remove_action('woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action('woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+add_action('woocommerce_before_main_content', function() {
+  echo '<div class="content-area test-container2">';
+}, 10);
+add_action('woocommerce_after_main_content', function() {
+  echo '</div>';
+}, 10);
+
+remove_action('woocommerce_before_main_content', 'woocommerce_breadcrumb', 20);
+add_action('woocommerce_before_main_content', 'woocommerce_breadcrumb', 9);
+add_action('woocommerce_before_shop_loop', 'woocommerce_breadcrumb', 11);
